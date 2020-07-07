e9c6bb410d0a09dcdc8604a6c98619721aaab897
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2020-07-07 17:05:40 -0300

    Filtro com Bootstrap e JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -105,4 +105,41 @@ $(document).ready(function () {
         $('#apply-area').parallax({imageSrc: './img/pattern.png'});
     }, 250);
 
+    // Filtro do portfolio
+
+    $('.filter-btn').on('click', function(){
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+
+        if(type === 'dsg-btn'){
+            eachBoxes('dsg', boxes);
+        }else if(type === 'dev-btn'){
+            eachBoxes('dev', boxes);
+        }else if (type === 'seo-btn'){
+            eachBoxes('seo', boxes);
+        }else{
+            eachBoxes('all', boxes);
+        }
+    });
+
+    function eachBoxes(type,boxes){
+        if(type === 'all'){
+            $(boxes).fadeIn();
+        }else{
+            $(boxes).each(function(){
+                if(!$(this).hasClass(type)){
+                    $(this).fadeOut('slow');
+                }else{
+                    $(this).fadeIn();
+                }
+            })
+        }
+    }
+
+
+
 });
